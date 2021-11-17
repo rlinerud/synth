11cf7070df4d3b8518cf2c9a8d90ac215b89bd6e
Rory Linerud <rlinerud@u.rochester.edu>
2021-11-17 13:48:30 -0500

    Generalize types to account for matrices

--- types.py
+++ types.py
@@ -3,36 +3,22 @@
 #
 # types.py
 
+from synth.matrix import Matrix
 from typing import Callable, Union
 from z3 import ArithRef, BoolRef
 
-# A `Variable` is a supported Z3 expression; a `Value` is a supported
-# Python primitive.
-Variable = Union[ArithRef, BoolRef]
-Value = Union[bool, int, float]
+Variable = Union[BoolRef, ArithRef]
+Value = Union[bool, int]
+Entry = Union[Variable, Value]
 
-# Sometimes it doesn't matter whether something is a Python primitive
-# or a Z3 variable.
 Boolean = Union[BoolRef, bool]
 Integral = Union[ArithRef, int]
-Real = Union[ArithRef, float]
-Arithmetic = Union[Integral, Real]
-Input = Union[Boolean, Arithmetic]
+#Real = Union[ArithRef, float]
+#Arithmetic = Union[Integral, Real]
+#Input = Union[Boolean, Arithmetic]
 
-# A `Library` is a collection of base components.
 Library = dict[str, int]
-
-# A `LineMapping` is a mapping from Z3 variables to their locations (or
-# line numbers) in a synthesized program.
-LineMapping = dict[Variable, Integral]
-
-# An `Oracle` is just a function that takes in supported inputs and
-# returns a supported output.
-Oracle = Callable[[Value, ...], Value]
-
-# An `Instantiator` is a Z3 function that instantiates a new Z3
-# variable, taking the new variable's name as input.
+LineMapping = dict[Matrix, Integral]
+Oracle = Callable[[Matrix, ...], Matrix]
 Instantiator = Callable[[str], Variable]
-
-# `Examples` is an abbreviation for a set of input-output pairs.
-Examples = dict[tuple[Value, ...], Value]
+Examples = dict[tuple[Matrix, ...], Matrix]
